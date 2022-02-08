cb0a6a5b243e8550a00bf857a5d457f09eaf2c13
imbolc <me@imbolc.name>
2022-02-08 15:42:28 +0300

    Update rust-analyzer

--- AppImages/update.sh
+++ AppImages/update.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
+
+# Rust Analyzer
+mkdir -p ~/.local/bin
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer
