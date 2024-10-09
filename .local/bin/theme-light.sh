0d9399e2bfd83aac95ac9cd971f8230fc009b923
imbolc <me@imbolc.name>
2024-10-09 06:50:59 +0600

    Bat theming

--- .local/bin/theme-light.sh
+++ .local/bin/theme-light.sh
@@ -6,3 +6,6 @@ xfconf-query -c xsettings -p /Net/ThemeName -s Adwaita
 cd "$HOME/.config/alacritty"
 ln -sf themes/solarized_light.toml current_theme.toml
 touch alacritty.toml
+
+cd "$HOME/.config/bat"
+ln -sf config-light config
