96082b607c29795fa47c669275d6f03c76463ee0
imbolc <me@imbolc.name>
2024-10-09 09:50:40 +0600

    themes

--- .local/bin/min-brightness.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o nounset  # error when referencing undefined variable
-set -o errexit  # exit when command fails
-
-sudo bash -c 'echo 8 > /sys/class/backlight/intel_backlight/brightness'
-
